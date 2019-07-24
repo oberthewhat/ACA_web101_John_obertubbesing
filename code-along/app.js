f5f5a9bd37d9d408c3548fefb8c264dcaed5a395
John Obertubbesing <oberthewhat@gmail.com>
2019-07-24 18:26:09 -0500

    adding tower and todo

---
+++ code-along/app.js
@@ -0,0 +1,6 @@
+var myName = "John Obertubbesing"
+
+var nameElement = document.getElementsByClassName('name')[0]
+nameElement.innerHTML = myName
+
+alert('this is an alert!')
---
+++ src/jQuery-todoapp/app.js
@@ -0,0 +1,22 @@
+$(function() {
+
+    let $list = $('ul');
+    let $newItemForm = $('#newItemForm');
+
+    $newItemForm.on('submit', function(e) {
+        e.preventDefault();
+        let text = $('input[type="text"]').val();
+        if (text == '') {
+            alert('Enter Item');
+        } else {
+            $list.append(`<li>${text}</li>`);
+            $('input[type="text"]').val('');
+        }
+    });
+
+    $list.on('click', 'li', function() {
+        let $this = $(this);
+        $this.remove();
+    });
+
+});
