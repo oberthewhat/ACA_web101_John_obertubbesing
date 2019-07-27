380ef474c923461ac21363a3d8e78aea130a565b
John Obertubbesing <oberthewhat@gmail.com>
2019-07-26 21:49:27 -0500

    fixed timer

--- src/final-check/app.js
+++ src/final-check/app.js
@@ -10,25 +10,24 @@ button.onclick = function() {
 
 window.onload = function() {
 
-    var second = 01;
+    var second = 60;
     var minute = 1;
 
     setInterval(function() {
-        document.getElementById("timer").innerHTML = minute + ":" + second;
+        document.getElementById("timer").innerHTML = "0:" + second;
 
         second--;
 
-        if (second == 00) {
-            minute--;
+        if (second == 60) {
             second = 60;
         }
-        if (minute == 0 && second == 1) {
+        if (second == 1) {
             document.getElementById("timer").innerHTML = "Buzz!";
             button.onclick = function() {
                 count = count;
             }
         }
-        if (minute <= -1) {
+        if (second <= -1) {
             document.getElementById("timer").innerHTML = "Final";
             button.onclick = function() {
                 count = count;
